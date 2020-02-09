60977a4d900473012a49a867b34072d5b4b958ef
Sheep named Bleep <bleepthesheep@gmail.com>
2020-02-09 16:02:46 +0600

    Updates

---
+++ precache-manifest.a66cc2ed204c567b16f17f2e88718409.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ba72aca0dec2df448cb66c856e7a601",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35f148359c3cd88f4e8f",
+    "url": "/static/css/main.3bf38736.chunk.css"
+  },
+  {
+    "revision": "0046b6ea2d5d8f03638d",
+    "url": "/static/js/2.26eaf7b6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.26eaf7b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35f148359c3cd88f4e8f",
+    "url": "/static/js/main.09cf3c72.chunk.js"
+  },
+  {
+    "revision": "8146f0cabe9c8f6aeb56",
+    "url": "/static/js/runtime-main.cad64008.js"
+  },
+  {
+    "revision": "fd9a84f0d9a8a6184933fa7b3bfbb0c6",
+    "url": "/static/media/logo.fd9a84f0.svg"
+  },
+  {
+    "revision": "06a469380cb09a64d372f773665e7500",
+    "url": "/static/media/noimage.06a46938.jpg"
+  },
+  {
+    "revision": "ddf3af69cf3f9166b2be51c84095a440",
+    "url": "/static/media/pause.ddf3af69.svg"
+  },
+  {
+    "revision": "1ca8f892112f736e38745c927e59ecb6",
+    "url": "/static/media/play.1ca8f892.svg"
+  }
+]);
